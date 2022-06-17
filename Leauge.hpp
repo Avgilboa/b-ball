17a4cd0b69682ae73cdc65035e9ac4b768b279a6
“Avgilboa” <gilboa.aviad@gmail.com>
2022-06-17 15:10:44 +0300

    update

--- Leauge.hpp
+++ Leauge.hpp
@@ -11,7 +11,6 @@
  *
  */
 class Schedule;
-<<<<<<< HEAD
 class League{
     private:
         std::vector<Team> _Teams;
@@ -27,24 +26,3 @@ class League{
         Schedule* schedule;
         std::vector<int> Game(double Home_Talent , double Away_Talent);
 };  
-=======
-class Schedule;
-
-class League
-{
-private:
-    std::vector<Team> _Teams;
-    bool _start;
-
-public:
-    League();
-    League &AddTeam(const Team &T);
-    League &AddTeam(std::string name, double talent);
-    bool StartLeague();
-    bool checkAvialbleName(const std::string &name) const;
-    void printTeams() const;
-    bool FillTeams();
-    Schedule* schedule;
-    std::vector<int> Game(double Home_Talent, double Away_Talent);
-};
->>>>>>> cb3698704121668aa182a924ebef30961964b9ac
--- Schedule.hpp
+++ Schedule.hpp
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
+#pragma once
 #include <algorithm>
 #include <deque>
 #include <map>
 #include <vector>
 #include <iostream>
-=======
-#pragma once
-
-#include "Leauge.hpp"
->>>>>>> cb3698704121668aa182a924ebef30961964b9ac
 /**
  * @file Schedule.hpp
  * @author Aviad Gilboa
